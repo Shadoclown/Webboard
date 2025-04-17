4686cebdab6f39030ea5e9854c2d8f6576adf97f
Shadoclown <dungky555@gmail.com>
2025-04-18 02:15:57 +0700

    move navbar
    move navbar from homepage to app.js

--- webboard/src/App.js
+++ webboard/src/App.js
@@ -1,11 +1,52 @@
 import React, { useEffect, useState } from 'react';
+import Login_page from './component/login_page';
+import Signup_page from './component/signup_page';
 import Homepage from './component/homepage';
+import './App.css';
 
 function App() {
   const [setPage, setPageState] = useState('Home');
+  const [showLogin, setShowLogin] = useState(false);
+  const [showSignup, setShowSignup] = useState(false);
+
   return (
     <div>
+      <nav className="navbar">
+        <a>
+            <div className="logo">Webboard</div>
+        </a>
+        <div className="search_bar">
+            <input type="search" placeholder="Search" />
+        </div>
+        <div className="button">
+            <button
+                className="login_btn"
+                id="btn"
+                onClick={() => setShowLogin(true)}>Login</button>
+            <button 
+                className="signup_btn" 
+                id="btn"
+                onClick={() => setShowSignup(true)}>Sign up</button>
+        </div>
+      </nav>
+
+        {showLogin && (
+            <div className="modal">
+                <Login_page 
+                    closePopup={() => setShowLogin(false)} 
+                    switchToSignup={() => {setShowLogin(false); setShowSignup(true)}} />
+            </div>
+        )}
+        {showSignup && (
+            <div className="modal">
+                <Signup_page 
+                    closePopup={() => setShowSignup(false)} 
+                    switchToLogin={() => {setShowSignup(false); setShowLogin(true)}} />
+            </div>
+        )}
+
       {setPage === 'Home' && <Homepage />}
+    
     </div>
   );
 }
--- webboard/src/component/homepage.js
+++ webboard/src/component/homepage.js
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import '../style_component/homepage.css';
 import All_content from './all_content';
-import Login_page from './login_page';
-import Signup_page from './signup_page';
 import { supabase } from './connect';
 
 
 function Homepage() {
     const [cardData, setCardData] = useState([]);
-    const [showLogin, setShowLogin] = useState(false);
-    const [showSignup, setShowSignup] = useState(false);
+    
 
     useEffect(() => {
         const fetchData = async () => {
@@ -32,41 +29,8 @@ function Homepage() {
 
     return (
         <div>
-            <nav className="navbar">
-                <a>
-                    <div className="logo">Webboard</div>
-                </a>
-                <div className="search_bar">
-                    <input type="search" placeholder="Search" />
-                </div>
-                <div className="button">
-                    <button
-                        className="login_btn"
-                        id="btn"
-                        onClick={() => setShowLogin(true)}>Login</button>
-                    <button 
-                        className="signup_btn" 
-                        id="btn"
-                        onClick={() => setShowSignup(true)}>Sign up</button>
-                </div>
-            </nav>
             <h3 className='choose'>Choose Your Favorite Category</h3>
             <All_content card_data={cardData} />
-
-            {showLogin && (
-                <div className="modal">
-                    <Login_page 
-                        closePopup={() => setShowLogin(false)} 
-                        switchToSignup={() => {setShowLogin(false); setShowSignup(true)}} />
-                </div>
-            )}
-            {showSignup && (
-                <div className="modal">
-                    <Signup_page 
-                        closePopup={() => setShowSignup(false)} 
-                        switchToLogin={() => {setShowSignup(false); setShowLogin(true)}} />
-                </div>
-            )}
         </div>
     );
 }
